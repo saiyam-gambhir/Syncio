c24f701fb6c86bd2e322e948959f5e87e445e72a
Saiyam Gambhir <saiyam@syncio.co>
2023-08-14 01:16:40 +0530

    Order push cleanup

--- src/views/orders/pushSettings/PushSettings.vue
+++ src/views/orders/pushSettings/PushSettings.vue
@@ -9,54 +9,28 @@ import PageHeader from '@/components/shared/PageHeader.vue';
 const { fetchPushSettings } = useOrders();
 const activeTabIndex = ref(0);
 
-/* ----- MOUNTED ----- */
+/* ----- Mounted ----- */
 onMounted(async () => {
   await fetchPushSettings();
 });
+
+/* ----- Methods ----- */
+const handleTabChange = async index => {
+  activeTabIndex.value = index;
+};
 </script>
 
 <template>
-  <PageHeader content="Manage order push settings" title="Order Push Settings">
-  </PageHeader>
+  <PageHeader content="Manage order push settings" title="Order Push Settings" />
 
   <section class="mt-4">
-    <ul class="surface-card p-0 m-0 list-none flex overflow-x-auto select-none">
-      <li class="pr-3">
-        <a class="cursor-pointer px-4 py-3 flex align-items-center border-bottom-2 hover:border-500 transition-colors transition-duration-150"
-          :class="{
-            'border-blue-500 text-blue-500 hover:border-blue-500':
-              activeTabIndex === 0,
-            'text-700 border-transparent': activeTabIndex !== 0,
-          }">
-          <span class="font-semibold capitalize">E-mail contact method</span>
-        </a>
-      </li>
-      <li class="flex align-items-center">
-        <div style="width: 1px; height: 50%" class="border-right-1 surface-border"></div>
-      </li>
-      <li class="px-3">
-        <a class="cursor-pointer px-4 py-3 flex align-items-center border-bottom-2 hover:border-500 transition-colors transition-duration-150"
-          :class="{
-            'border-blue-500 text-blue-500 hover:border-blue-500':
-              activeTabIndex === 1,
-            'text-700 border-transparent': activeTabIndex !== 1,
-          }">
-          <span class="font-semibold capitalize">Shipping rules rates</span>
-        </a>
-      </li>
-      <li class="flex align-items-center">
-        <div style="width: 1px; height: 50%" class="border-right-1 surface-border"></div>
-      </li>
-      <li class="pl-3">
-        <a class="cursor-pointer px-4 py-3 flex align-items-center border-bottom-2 hover:border-500 transition-colors transition-duration-150"
-          :class="{
-            'border-blue-500 text-blue-500 hover:border-blue-500':
-              activeTabIndex === 2,
-            'text-700 border-transparent': activeTabIndex !== 2,
-          }">
-          <span class="font-semibold capitalize">Shipping type tags</span>
-        </a>
-      </li>
-    </ul>
+    <TabView @update:activeIndex="handleTabChange" class="mt-4 margin-bottom">
+      <TabPanel header="E-mail contact method">
+      </TabPanel>
+      <TabPanel header="Shipping rules rates">
+      </TabPanel>
+      <TabPanel header="Shipping type tags">
+      </TabPanel>
+    </TabView>
   </section>
 </template>
