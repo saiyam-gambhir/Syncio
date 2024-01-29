526092c0051c38ce1b86011222686d15ebb1cc66
Saiyam Gambhir <saiyam1290@gmail.com>
2024-01-29 10:03:52 +0530

    Save default commission fix

--- src/stores/payoutsSettings/actions/updateStoreCommission.js
+++ src/stores/payoutsSettings/actions/updateStoreCommission.js
@@ -1,11 +1,11 @@
 export const updateStoreCommission = {
   async updateStoreCommission() {
     const { type, value } = this.storeDefaultCommissionRate;
-    const { storeId } = useConnectionsStore();
+    const { storeId, fetchCurrentStore } = useConnectionsStore();
 
     const params = {
       commission_type: type,
-      commission_value: value,
+      commission_value: String(value),
       connection_id: null,
       current_store_id: storeId,
       target_store_id: null,
@@ -14,6 +14,7 @@ export const updateStoreCommission = {
     const response = await axiosService.postData(`stores/${storeId}/add-store-commission`, params);
     if(response.success) {
       this.products = response.products;
+      fetchCurrentStore();
     }
   }
 };
