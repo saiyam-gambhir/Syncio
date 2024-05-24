d3bf89dc90c707e0d1ec102ff5b8d4d5e216489b
Saiyam Gambhir <saiyam@syncio.co>
2024-05-24 09:57:09 +0530

    Connect your store page design

--- src/views/registration/shopify/ConnectYourFirstStore.vue
+++ src/views/registration/shopify/ConnectYourFirstStore.vue
@@ -7,7 +7,24 @@ const ConnectNewStoreDialog = defineAsyncComponent(() => import('../../connectio
 /* ----- Data ----- */
 const isTestStoreConnected = ref(false);
 const loading = ref(false);
-
+const selectedOption = ref('');
+const options = ref([
+  {
+    btnLabel: 'I received a Destination store key',
+    description: null,
+    type: 'uniqueKey',
+  },
+  {
+    btnLabel: 'Share your key via email ',
+    description: null,
+    type: 'email',
+  },
+  {
+    btnLabel: 'Create a Marketplace profile',
+    description: 'Allows you to browse and connect with quality partner stores. Select this option if you don’t yet have a store to connect with.',
+    type: 'profile',
+  },
+]);
 const {
   connectPartnerStore,
   isConnectViaStoreKeyRequested,
@@ -43,16 +60,11 @@ const showConnectViaKeyHandler = () => {
 </script>
 
 <template>
-  <section class="mx-auto" style="width: 900px;">
-    <PageDetails :title="`Connect to your first ${partnerStoreType}!`" content="" />
+  <section class="mx-auto" style="width: 850px;">
+    <PageDetails :title="`Connect to your first ${partnerStoreType}`" content="" />
 
-    <aside class="auth-wrapper text-900 text-center">
-      <h3>Your store's unique key:</h3>
-      <h3 class="text-4xl font-semi mb-4" :style="{ color: isDestinationStore ? '#fa757b' : '#f89117' }" style="letter-spacing: 1.5px;">{{ storeKey || '658adff94608e' }}</h3>
-      <p class="text-lg line-height-3 m-0" v-if="isDestinationStore">Share your unique key with source stores to start importing their products to sell on your store.</p>
-      <p class="text-lg line-height-3 m-0" v-else>Share your unique key with destination stores to allow them to import your products to sell on their store.</p>
-      <p class="text-lg line-height-3 mt-1">This unique key can also be found on your dashboard after completing this setup process.</p>
-      <div class="grid pt-4 pb-3">
+    <aside class="auth-wrapper text-900">
+      <!-- <div class="grid pt-4 pb-3">
         <div class="col-6">
           <Button
             @click="showInviteViaEmailHandler"
@@ -72,9 +84,9 @@ const showConnectViaKeyHandler = () => {
             outlined>
           </Button>
         </div>
-      </div>
+      </div> -->
 
-      <template v-if="!isTestStoreConnected && isDestinationStore">
+      <!-- <template v-if="!isTestStoreConnected && isDestinationStore">
         <Divider />
         <p class="text-lg line-height-3 mt-3 mb-0">Haven't received a unique key?</p>
         <p class="text-lg line-height-3 mt-1">Why not try out our awesome features with our test store instead?</p>
@@ -87,15 +99,36 @@ const showConnectViaKeyHandler = () => {
           label="Connect to test store"
           outlined>
         </Button>
-      </template>
+      </template> -->
+
+      <h3 class="text-2xl mb-2">How do you want to connect?</h3>
+      <p class="text-lg line-height-3">Syncio uses Unique Store Keys to establish connections between stores. <br> Find your Unique Store Key at any time on the in app dashboard.</p>
+      <ul class="m-0 pt-3 p-0 radio-list">
+        <li v-for="({ btnLabel, description, type }, index) in options" :key="type" class="flex align-items-center relative p-4 border-1 border-400" :class="{ 'selected' : selectedOption === type, 'border-top-none' : index > 0 }">
+          <RadioButton v-model="selectedOption" :inputId="type" name="dynamic" :value="type" class="absolute" />
+          <label class="flex flex-column pr-4 pl-6 text-lg pointer" :for="type">
+            {{ btnLabel }}
+            <template v-if="description">
+              <span class="mt-2 line-height-3 text-sm">
+                {{ description }}
+              </span>
+            </template>
+          </label>
+        </li>
+      </ul>
+
+      <div class="border-top-1 border-400 my-6"></div>
+
+      <div class="text-center">
+        <div class="flex align-items-center justify-content-end">
+          <router-link :to="isDestinationStore ? routes.SHOPIFY_SELECT_PLAN : routes.SHOPIFY_INSTALLATION_COMPLETE">
+            <a href="javascript:void(0);" class="btn-link mr-5 text-lg">Skip</a>
+          </router-link>
+          <Button label="Next" class="font-bold justify-content-center"></Button>
+        </div>
+      </div>
 
     </aside>
-
-    <div class="text-center">
-      <router-link :to="isDestinationStore ? routes.SHOPIFY_SELECT_PLAN : routes.SHOPIFY_INSTALLATION_COMPLETE">
-        <Button @click="loading = true" :loading="loading" label="Skip for now" class="my-6 font-bold justify-content-center p-button-lg"></Button>
-      </router-link>
-    </div>
   </section>
 
   <ConnectNewStoreDialog v-if="isNewStoreConnectionRequested" :enableBackBtn="false" />
