5f2a96f7d62e643c7bfbed33b627ab1fe75541cf
Suharsh Nagasampagi <suharsh@syncio.co>
2024-09-10 16:17:35 +1000

    Update variables

--- src/components/navbar/PlanUsageIndicator.vue
+++ src/components/navbar/PlanUsageIndicator.vue
@@ -6,15 +6,15 @@ const {
 
 /* ----- Computed ----- */
 const getPlanDetails = computed(() => {
-  const sy = +plan.value?.synced_orders || 0;
+  const syncedOrders = +plan.value?.synced_orders || 0;
 
-  if (sy > 1000) {
+  if (syncedOrders > 1000) {
     return { title: 'Enterprise', availableLimit: 10000 };
   }
-  if (sy > 100) {
+  if (syncedOrders > 100) {
     return { title: 'Pro', availableLimit: 1000 };
   }
-  if (sy > 10) {
+  if (syncedOrders > 10) {
     return { title: 'Starter', availableLimit: 100 };
   }
 
@@ -25,7 +25,7 @@ const getPlanDetails = computed(() => {
 <template>
   <div class="mt-auto p-3" style="background: #f8f9fa;" v-if="plan?.active_addons.length === 0">
     <div class="border-round shadow-2 surface-0 p-3">
-      <PlanUsage :title="getPlanDetails.title" :limitUsed="+plan?.synced_products_sold || 0" :limitAvailable="getPlanDetails.availableLimit"></PlanUsage>
+      <PlanUsage :title="getPlanDetails.title" :limitUsed="+plan?.synced_orders || 0" :limitAvailable="getPlanDetails.availableLimit"></PlanUsage>
     </div>
   </div>
 </template>
